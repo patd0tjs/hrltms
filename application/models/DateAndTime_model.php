9f961144b8400f2c0e2523539095b319ccf249cb
Patrick Glenn Balanza <pgbalanza@gmail.com>
2022-12-05 11:28:08 +0800

    add designations

--- application/models/DateAndTime_model.php
+++ application/models/DateAndTime_model.php
@@ -17,12 +17,15 @@ class DateAndTime_model extends CI_Model{
                         ->select('employees.l_name as l_name')
                         ->select('employees.f_name as f_name')
                         ->select('employees.m_name as m_name')
+                        ->select('designations.name as designation')
                         ->select('schedule.s_date as s_date')
                         ->select('schedule.e_date as e_date')
                         ->select('schedule.time_in as time_in')
                         ->select('schedule.time_out as time_out')
                         ->from('schedule')
                         ->join('employees', 'schedule.emp_id=employees.id')
+                        ->join('employee_details', 'employees.id=employee_details.id')
+                        ->join('designations', 'employee_details.designation_id=designations.id')
                         ->get()
                         ->result_array();
     }
@@ -132,12 +135,15 @@ class DateAndTime_model extends CI_Model{
                         ->select('employees.l_name as l_name')
                         ->select('employees.f_name as f_name')
                         ->select('employees.m_name as m_name')
+                        ->select('designations.name as designation')
                         ->select('dtr.s_date as s_date')
                         ->select('dtr.e_date as e_date')
                         ->select('dtr.time_in as time_in')
                         ->select('dtr.time_out as time_out')
                         ->from('dtr')
                         ->join('employees', 'dtr.emp_id=employees.id')
+                        ->join('employee_details', 'employees.id=employee_details.id')
+                        ->join('designations', 'employee_details.designation_id=designations.id')
                         ->get()
                         ->result_array();
     }
