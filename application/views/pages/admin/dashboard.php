b5c30b9d968ae5ca877494002563faa7f00520bd
Patrick Glenn Balanza <pgbalanza@gmail.com>
2023-01-01 10:48:01 +0800

    dashboard charts

--- application/views/pages/admin/dashboard.php
+++ application/views/pages/admin/dashboard.php
@@ -1,3 +1,5 @@
+<script src="https://code.highcharts.com/highcharts.js"></script>
+
 <div class="container overflow-hidden">
   <div class="row gx-5">
     <div class="col">
@@ -20,3 +22,56 @@
     </div>
   </div>
 </div>
+
+<figure class="highcharts-figure">
+  <div id="container"></div>
+</figure>
+
+<script>
+
+Highcharts.chart('container', {
+  chart: {
+    plotBackgroundColor: null,
+    plotBorderWidth: null,
+    plotShadow: false,
+    type: 'pie'
+  },
+  title: {
+    text: 'Overall count'
+  },
+  tooltip: {
+    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+  },
+  accessibility: {
+    point: {
+      valueSuffix: '%'
+    }
+  },
+  plotOptions: {
+    pie: {
+      allowPointSelect: true,
+      cursor: 'pointer',
+      dataLabels: {
+        enabled: true,
+        format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+      }
+    }
+  },
+  series: [{
+    name: 'Count',
+    colorByPoint: true,
+    data: [{
+      name: 'Employees',
+      y: <?= count($employees)?>,
+      sliced: true,
+      selected: true
+    }, {
+      name: 'Approved Leaves',
+      y: <?= count($leaves)?>
+    },  {
+      name: 'Tardies',
+      y: <?= count($tardies)?>
+    }]
+  }]
+});
+</script>
